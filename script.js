69f88cdbdc790b34e4bdf61b7eb4a7afa005e163
BIKI KUMAR SINGH <141229332+bikikumarsingh@users.noreply.github.com>
2025-01-22 18:26:20 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,40 @@
+async function searchWeather() {
+    const city = document.getElementById('city-input').value;
+    const apiKey = 'eb89d565a868b1acdb04430e464be646'; 
+    const apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
+
+    try {
+        const response = await fetch(apiUrl);
+        if (!response.ok) {
+            throw new Error('City not found');
+        }
+        const data = await response.json();
+        displayWeather(data);
+    } catch (error) {
+        alert(error.message);
+    }
+}
+
+function displayWeather(data) {
+    const weatherContainer = document.getElementById('weather-container');
+
+    
+    weatherContainer.innerHTML = `
+        <h2>${data.name}, ${data.sys.country}</h2>
+        <p>Temperature: ${data.main.temp}°C</p>
+        <p>Weather: ${data.weather[0].description}</p>
+        <p>Humidity: ${data.main.humidity}%</p>
+        <p>Wind Speed: ${data.wind.speed} m/s</p>
+        <button onclick="removeWeather()">Remove</button>
+    `;
+
+    
+    weatherContainer.classList.add('show');
+}
+
+function removeWeather() {
+    const weatherContainer = document.getElementById('weather-container');
+    if (weatherContainer) {
+        weatherContainer.classList.remove('show');
+    }
+}
